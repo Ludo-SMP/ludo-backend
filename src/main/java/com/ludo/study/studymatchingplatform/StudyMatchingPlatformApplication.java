52ab07f880b9ee1440aa335f5bf7b1057115ed55
Hugh <lee2317884@gmail.com>
2024-01-04 20:34:53 +0900

    init: Initial commitment to the project

---
+++ src/main/java/com/ludo/study/studymatchingplatform/StudyMatchingPlatformApplication.java
@@ -0,0 +1,11 @@
+package com.ludo.study.studymatchingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMatchingPlatformApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMatchingPlatformApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ludo/study/studymatchingplatform/StudyMatchingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludo.study.studymatchingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMatchingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
