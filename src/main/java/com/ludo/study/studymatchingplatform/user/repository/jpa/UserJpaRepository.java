106a797c2fedb08bf479bca980e7d24d27695baa
Hugh <lee2317884@gmail.com>
2024-01-26 20:59:26 +0900

    feat: spring data jpa 인터페이스 및 공통 repository 인터페이스

--- src/main/java/com/ludo/study/studymatchingplatform/user/repository/jpa/UserJpaRepository.java
+++ src/main/java/com/ludo/study/studymatchingplatform/user/repository/jpa/UserJpaRepository.java
@@ -1,4 +1,4 @@
-package com.ludo.study.studymatchingplatform.study.repository.jpa;
+package com.ludo.study.studymatchingplatform.user.repository.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
