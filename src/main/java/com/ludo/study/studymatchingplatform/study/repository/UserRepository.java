7a858a8a0973e0e17885e97f96bc08d3754b5bb5
june-777 <wlwhswnsrl96@gmail.com>
2024-01-28 21:48:41 +0900

    remove: 중복으로 존재하는 repository 클래스 제거
    RecruitmentRepositoryImpl 은 spring data jpa repository 로 통합

--- src/main/java/com/ludo/study/studymatchingplatform/study/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ludo.study.studymatchingplatform.study.repository;
-
-import com.ludo.study.studymatchingplatform.user.domain.User;
-
-public interface UserRepository {
-
-	User save(User user);
-
-}
