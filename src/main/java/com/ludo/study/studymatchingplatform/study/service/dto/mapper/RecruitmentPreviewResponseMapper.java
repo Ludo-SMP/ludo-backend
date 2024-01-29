a9eade13b09efd4e6554ef288738bda05c68136f
june-777 <wlwhswnsrl96@gmail.com>
2024-01-29 16:25:31 +0900

    feat: 모집 공고 Recruitment를 모집 공고 미리보기 RecruitmentPreviewResponse로 변환하는 Mapper 클래스 구현

---
+++ src/main/java/com/ludo/study/studymatchingplatform/study/service/dto/mapper/RecruitmentPreviewResponseMapper.java
@@ -0,0 +1,40 @@
+package com.ludo.study.studymatchingplatform.study.service.dto.mapper;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.ludo.study.studymatchingplatform.study.domain.Study;
+import com.ludo.study.studymatchingplatform.study.domain.recruitment.Recruitment;
+import com.ludo.study.studymatchingplatform.study.service.dto.response.RecruitmentPreviewResponse;
+
+@Component
+public class RecruitmentPreviewResponseMapper {
+
+	public List<RecruitmentPreviewResponse> mapBy(final List<Recruitment> recruitments) {
+		return recruitments.stream()
+			.map(recruitment -> {
+				Study study = recruitment.getStudy();
+				return getRecruitmentPreviewResponse(recruitment, study);
+			})
+			.toList();
+	}
+
+	private RecruitmentPreviewResponse getRecruitmentPreviewResponse(final Recruitment recruitment, final Study study) {
+		return new RecruitmentPreviewResponse(
+			recruitment.getId(),
+			recruitment.getTitle(),
+			recruitment.getCreatedDateTime().toString(),
+			recruitment.getRecruitmentEndDateTime().toString(),
+			recruitment.getHits(),
+			recruitment.getStackNames(),
+			recruitment.getPositionNames(),
+
+			study.getCategoryByName(),
+			study.getOwnerNickname(),
+			study.getWay().toString(),
+			study.getStartDateTime().toString(),
+			study.getEndDateTime().toString()
+		);
+	}
+}
