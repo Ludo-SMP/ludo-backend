864cc3b01f607ef8878f0c8a36faf2deb8c82f1d
Hugh <lee2317884@gmail.com>
2024-01-31 19:43:45 +0900

    SP-213 feat: QuerydslConfig 추가

---
+++ src/main/java/com/ludo/study/studymatchingplatform/config/QuerydslConfig.java
@@ -0,0 +1,13 @@
+package com.ludo.study.studymatchingplatform.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfig {
+
+	// @Bean
+	// public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+	// 	return new JPAQueryFactory(entityManager);
+	// }
+
+}
