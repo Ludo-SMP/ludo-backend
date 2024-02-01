8ab389b4bae04e8211c0052dbec2e7e1212ed34c
Hugh <lee2317884@gmail.com>
2024-02-01 12:42:18 +0900

    SP-213 feat: controller 구현
    - 빌터 패턴 적용
    - 초기에는 dto 내부에 builder 메서드를 위치했으나, dto는 순수한 데이터 전달의 역할만 담당하는것이 자연스럽다고 판단하여 별도의 클래스로 구성

--- src/main/java/com/ludo/study/studymatchingplatform/study/controller/StudyController.java
+++ src/main/java/com/ludo/study/studymatchingplatform/study/controller/StudyController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,9 +25,9 @@ public class StudyController {
 	private final RecruitmentDeleteService recruitmentCreateService;
 
 	@PostMapping
-	public ResponseEntity<Void> create(final StudyCreateRequest request) {
-		final Long studyId = studyCreateService.create(request);
-		return ResponseEntity.created(URI.create("/api/studies/" + studyId)).build();
+	public ResponseEntity<Void> create(@RequestBody final StudyCreateRequest request) {
+		studyCreateService.create(request);
+		return ResponseEntity.created(URI.create("/api")).build();
 	}
 
 	@DeleteMapping("/{studyId}/recruitments")
