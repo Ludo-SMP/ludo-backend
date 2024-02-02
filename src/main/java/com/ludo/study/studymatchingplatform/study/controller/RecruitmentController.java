3d8b0a0b1a316275e13f0983418e90409c242d2e
june-777 <wlwhswnsrl96@gmail.com>
2024-02-02 14:55:21 +0900

    SP-219 feat: 지원 공고 목록 조회 컨트롤러 구현
    지원 공고 목록 api 명세에 맞게 지원 공고 목록 결과를 래핑하여 반환

--- src/main/java/com/ludo/study/studymatchingplatform/study/controller/RecruitmentController.java
+++ src/main/java/com/ludo/study/studymatchingplatform/study/controller/RecruitmentController.java
@@ -1,14 +1,20 @@
 package com.ludo.study.studymatchingplatform.study.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ludo.study.studymatchingplatform.study.service.RecruitmentDetailsFindService;
+import com.ludo.study.studymatchingplatform.study.service.RecruitmentsFindService;
 import com.ludo.study.studymatchingplatform.study.service.dto.response.RecruitmentDetailsResponse;
+import com.ludo.study.studymatchingplatform.study.service.dto.response.RecruitmentPreviewResponse;
+import com.ludo.study.studymatchingplatform.study.service.dto.response.RecruitmentPreviewResponses;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +26,16 @@ import lombok.extern.slf4j.Slf4j;
 public class RecruitmentController {
 
 	private final RecruitmentDetailsFindService recruitmentDetailsFindService;
+	private final RecruitmentsFindService recruitmentsFindService;
+
+	@GetMapping
+	public ResponseEntity<RecruitmentPreviewResponses> readRecruitments(
+		@RequestParam(required = false) Integer after, @RequestParam Integer count
+	) {
+		List<RecruitmentPreviewResponse> recruitments = recruitmentsFindService.findRecruitments(after, count);
+
+		return ResponseEntity.ok().body(new RecruitmentPreviewResponses(recruitments));
+	}
 
 	@GetMapping("/{recruitment-id}")
 	public ResponseEntity<RecruitmentDetailsResponse> readRecruitmentDetails(
