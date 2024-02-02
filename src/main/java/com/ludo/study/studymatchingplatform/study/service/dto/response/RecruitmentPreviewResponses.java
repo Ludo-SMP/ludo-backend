95747b3eba8089477e63fe33d555bb15ff7659da
june-777 <wlwhswnsrl96@gmail.com>
2024-02-02 14:56:02 +0900

    SP-219 feat: response dto, mapper 클래스 구현

---
+++ src/main/java/com/ludo/study/studymatchingplatform/study/service/dto/response/RecruitmentPreviewResponses.java
@@ -0,0 +1,7 @@
+package com.ludo.study.studymatchingplatform.study.service.dto.response;
+
+import java.util.List;
+
+public record RecruitmentPreviewResponses(List<RecruitmentPreviewResponse> recruitments) {
+
+}
