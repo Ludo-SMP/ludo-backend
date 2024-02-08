ae0a47341c4c6d464be953304b9709c93222ec0a
june-777 <wlwhswnsrl96@gmail.com>
2024-02-09 04:21:48 +0900

    SP-233 feat: User를 저장하는 기능, 이메일로 찾는 기능

--- src/main/java/com/ludo/study/studymatchingplatform/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ludo.study.studymatchingplatform.user.repository;
-
-import com.ludo.study.studymatchingplatform.user.domain.User;
-
-public interface UserRepository {
-
-	User save(final User user);
-
-}
