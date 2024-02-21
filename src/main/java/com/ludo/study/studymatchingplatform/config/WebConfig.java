0b223d64cf5a1ab627bf5113d5e2e85020036f77
Hugh <lee2317884@gmail.com>
2024-02-21 21:16:32 +0900

    hotfix: 로컬 프론트엔드와 백엔드 서버의 서드파티 쿠키 문제 해결
    - 양쪽 서버 https 적용
    - 도메인 네임 일치
    - 쿠키 설정 변경
      - secure true
      - sameSite None
      - httpOnly true

--- src/main/java/com/ludo/study/studymatchingplatform/config/WebConfig.java
+++ src/main/java/com/ludo/study/studymatchingplatform/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("http://localhost:3000")
+				.allowedOrigins("https://local.ludoapi.store:3000")
 				.allowedMethods("*")
 				.allowedHeaders("*")
 				.allowCredentials(true)
