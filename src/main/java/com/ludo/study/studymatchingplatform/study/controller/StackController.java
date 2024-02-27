4480e39d45c0585e141ca982e238ca7125b5763d
june-777 <wlwhswnsrl96@gmail.com>
2024-02-28 02:23:06 +0900

    feat: 기술스택 조회 api response에 맞게 로직 추가

--- src/main/java/com/ludo/study/studymatchingplatform/study/controller/StackController.java
+++ src/main/java/com/ludo/study/studymatchingplatform/study/controller/StackController.java
@@ -6,9 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ludo.study.studymatchingplatform.study.controller.dto.response.BaseApiResponse;
 import com.ludo.study.studymatchingplatform.study.domain.stack.Stack;
 import com.ludo.study.studymatchingplatform.study.repository.stack.StackRepositoryImpl;
-import com.ludo.study.studymatchingplatform.study.service.dto.response.AllStackFindApiResponse;
 import com.ludo.study.studymatchingplatform.study.service.dto.response.StackResponses;
 
 import lombok.RequiredArgsConstructor;
@@ -21,12 +21,11 @@ public class StackController {
 
 	private final StackRepositoryImpl stackRepository;
 
-	@GetMapping("/api/stacks")
-	public ResponseEntity<AllStackFindApiResponse> getStacks() {
+	@GetMapping("/stacks")
+	public ResponseEntity<BaseApiResponse<StackResponses>> getStacks() {
 		List<Stack> stacks = stackRepository.findAll();
 		StackResponses stackResponses = new StackResponses(stacks);
-		AllStackFindApiResponse allStackResponse = new AllStackFindApiResponse(true, "기술 스택 조회 성공", stackResponses);
-		return ResponseEntity.ok(allStackResponse);
+		return ResponseEntity.ok(BaseApiResponse.success("기술 스택 조회 성공", stackResponses));
 	}
 
 }
