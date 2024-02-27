6771e0b6a1cce9c87f276e014d0e81795629db3b
june-777 <wlwhswnsrl96@gmail.com>
2024-02-27 12:41:20 +0900

    feat: Applicant, Participant 레포지토리 find 구현
    - 아직 관련 기능은 없지만, 추 후 존재할 가능성 존재
    - 테스트 코드에서 활용하기 위함

--- src/main/java/com/ludo/study/studymatchingplatform/study/repository/ParticipantRepositoryImpl.java
+++ src/main/java/com/ludo/study/studymatchingplatform/study/repository/ParticipantRepositoryImpl.java
@@ -1,8 +1,13 @@
 package com.ludo.study.studymatchingplatform.study.repository;
 
+import static com.ludo.study.studymatchingplatform.study.domain.QParticipant.*;
+
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 import com.ludo.study.studymatchingplatform.study.domain.Participant;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,9 +16,18 @@ import lombok.RequiredArgsConstructor;
 public class ParticipantRepositoryImpl {
 
 	private final ParticipantJpaRepository participantJpaRepository;
+	private final JPAQueryFactory q;
 
 	public Participant save(final Participant participant) {
 		return participantJpaRepository.save(participant);
+	}
+
+	public Optional<Participant> find(final Long studyId, final Long userId) {
+		return Optional.ofNullable(q.selectFrom(participant)
+				.where(participant.study.id.eq(studyId))
+				.where(participant.user.id.eq(userId))
+				.fetchOne()
+		);
 	}
 
 }
