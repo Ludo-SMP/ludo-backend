13f0d44760b0ad5671b6dd3593ecbceb6638ba7d
june-777 <wlwhswnsrl96@gmail.com>
2024-02-27 12:42:39 +0900

    feat: ApplicantId 클래스에 애노테이션 추가

--- src/main/java/com/ludo/study/studymatchingplatform/study/domain/recruitment/id/ApplicantId.java
+++ src/main/java/com/ludo/study/studymatchingplatform/study/domain/recruitment/id/ApplicantId.java
@@ -3,10 +3,16 @@ package com.ludo.study.studymatchingplatform.study.domain.recruitment.id;
 import java.io.Serializable;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Embeddable
 @EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ApplicantId implements Serializable {
 
 	private Long recruitmentId;
