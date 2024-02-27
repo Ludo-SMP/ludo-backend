790805bcfcd402072525eb6b6f5f142e3574a432
june-777 <wlwhswnsrl96@gmail.com>
2024-02-27 18:29:49 +0900

    test: 지원자 거절 서비스 계층 테스트

--- src/test/java/com/ludo/study/studymatchingplatform/study/service/StudyApplicantDecisionServiceTest.java
+++ src/test/java/com/ludo/study/studymatchingplatform/study/service/StudyApplicantDecisionServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,6 +27,7 @@ import com.ludo.study.studymatchingplatform.study.repository.StudyRepositoryImpl
 import com.ludo.study.studymatchingplatform.study.repository.recruitment.ApplicantRepositoryImpl;
 import com.ludo.study.studymatchingplatform.study.repository.recruitment.RecruitmentRepositoryImpl;
 import com.ludo.study.studymatchingplatform.study.service.dto.request.StudyApplicantDecisionRequest;
+import com.ludo.study.studymatchingplatform.study.service.dto.response.ParticipantResponse;
 import com.ludo.study.studymatchingplatform.user.domain.Social;
 import com.ludo.study.studymatchingplatform.user.domain.User;
 import com.ludo.study.studymatchingplatform.user.repository.UserRepositoryImpl;
@@ -61,7 +63,8 @@ class StudyApplicantDecisionServiceTest {
 
 	@Test
 	@Transactional
-	void successTest() {
+	@DisplayName("[Success] 스터디 지원자 수락 성공")
+	void applicantAcceptTest() {
 		// given
 		User owner = UserFixture.createUser(Social.NAVER, "archa", "archa@gmail.com");
 		User other = UserFixture.createUser(Social.NAVER, "other", "other@gmail.com");
@@ -85,23 +88,65 @@ class StudyApplicantDecisionServiceTest {
 		// when
 		StudyApplicantDecisionRequest studyApplicantDecisionRequest = new StudyApplicantDecisionRequest(study.getId(),
 				recruitment.getId(), other.getId());
-		applicantDecisionService.applicantAccept(owner, studyApplicantDecisionRequest);
+		ParticipantResponse participantResponse = applicantDecisionService.applicantAccept(owner,
+				studyApplicantDecisionRequest);
 
 		// then
+		assertParticipantResponse(participantResponse, other);
 		Applicant findApplicant = applicantRepository.find(recruitment.getId(), other.getId()).get();
-		assertApplicant(findApplicant);
+		assertApplicantStatus(findApplicant, ApplicantStatus.ACCEPTED);
 
 		Optional<Participant> findParticipant = participantRepository.find(study.getId(), other.getId());
 		assertParticipant(findParticipant, other, study);
 	}
 
-	private static void assertApplicant(Applicant findApplicant) {
-		assertThat(findApplicant.getApplicantStatus()).isEqualTo(ApplicantStatus.ACCEPTED);
+	@Test
+	@Transactional
+	@DisplayName("[Success] 스터디 지원자 거절 성공")
+	void applicantRejectTest() {
+		// given
+		User owner = UserFixture.createUser(Social.NAVER, "archa", "archa@gmail.com");
+		User other = UserFixture.createUser(Social.NAVER, "other", "other@gmail.com");
+		saveUsers(owner, other);
+
+		Category project = saveCategory();
+
+		Study study = StudyFixture.createStudy("스터디A", project, owner, 5);
+		Recruitment recruitment = RecruitmentFixture.createRecruitment(study, "지원공고", "내용", 5, "callurl", null);
+		study.registerRecruitment(recruitment);
+
+		Applicant applicant = ApplicantFixture.createApplicant(other, null);
+		recruitment.addApplicant(applicant);
+
+		studyRepository.save(study);
+		recruitmentRepository.save(recruitment);
+		applicantRepository.save(applicant);
+
+		entityManager.flush();
+
+		// when
+		StudyApplicantDecisionRequest studyApplicantDecisionRequest = new StudyApplicantDecisionRequest(study.getId(),
+				recruitment.getId(), other.getId());
+		applicantDecisionService.applicantReject(owner, studyApplicantDecisionRequest);
+
+		// then
+		Applicant findApplicant = applicantRepository.find(recruitment.getId(), other.getId()).get();
+		assertApplicantStatus(findApplicant, ApplicantStatus.REJECTED);
 	}
 
-	private static void assertParticipant(Optional<Participant> findParticipant, User other, Study study) {
+	private void assertParticipantResponse(ParticipantResponse participantResponse, User other) {
+		assertThat(participantResponse.user().nickname()).isEqualTo("other");
+		assertThat(participantResponse.user().email()).isEqualTo("other@gmail.com");
+		assertThat(participantResponse.user().id()).isEqualTo(other.getId());
+	}
+
+	private static void assertApplicantStatus(Applicant findApplicant, ApplicantStatus applicantStatus) {
+		assertThat(findApplicant.getApplicantStatus()).isEqualTo(applicantStatus);
+	}
+
+	private static void assertParticipant(Optional<Participant> findParticipant, User user, Study study) {
 		assertThat(findParticipant).isPresent();
-		assertThat(findParticipant.get().getUser()).isEqualTo(other);
+		assertThat(findParticipant.get().getUser()).isEqualTo(user);
 		assertThat(findParticipant.get().getStudy()).isEqualTo(study);
 		assertThat(study.getParticipants()).isNotEmpty();
 	}
