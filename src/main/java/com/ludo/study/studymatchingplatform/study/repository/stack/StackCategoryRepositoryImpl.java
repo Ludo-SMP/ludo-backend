07327a8ec1f2bea949ef88626356834ab36236b7
Hugh <lee2317884@gmail.com>
2024-03-03 14:46:19 +0900

    SP-347 refactor: 기술스택 API 응답 항목 최적화
    - 응답 시간 향상 목적

--- src/main/java/com/ludo/study/studymatchingplatform/study/repository/stack/StackCategoryRepositoryImpl.java
+++ src/main/java/com/ludo/study/studymatchingplatform/study/repository/stack/StackCategoryRepositoryImpl.java
@@ -1,8 +1,13 @@
 package com.ludo.study.studymatchingplatform.study.repository.stack;
 
+import static com.ludo.study.studymatchingplatform.study.domain.stack.QStackCategory.*;
+
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.ludo.study.studymatchingplatform.study.domain.stack.StackCategory;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,10 +15,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class StackCategoryRepositoryImpl {
 
+	private final JPAQueryFactory q;
 	private final StackCategoryJpaRepository stackCategoryJpaRepository;
 
 	public StackCategory save(final StackCategory stackCategory) {
 		return stackCategoryJpaRepository.save(stackCategory);
+	}
+
+	public List<StackCategory> findAll() {
+		return q.selectFrom(stackCategory)
+				.where(stackCategory.name.in("프론트엔드", "백엔드", "디자이너", "데이터베이스", "데브옵스", "언어"))
+				.fetch();
 	}
 
 }
