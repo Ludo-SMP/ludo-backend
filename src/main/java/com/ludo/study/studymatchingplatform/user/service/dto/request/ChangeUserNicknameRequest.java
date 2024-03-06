ad03255e101a429fdf8d51694ef8c27df8d642f5
june-777 <wlwhswnsrl96@gmail.com>
2024-03-06 18:26:52 +0900

    feat: 유저 닉네임 변경 요청에 대한 Bean Validation 추가
    닉네임 비즈니스 요구사항
    1. 빈 문자 x
    2. 공백 문자 x
    3. 맨 앞/뒤 공백 문자 x

--- src/main/java/com/ludo/study/studymatchingplatform/user/service/dto/request/ChangeUserNicknameRequest.java
+++ src/main/java/com/ludo/study/studymatchingplatform/user/service/dto/request/ChangeUserNicknameRequest.java
@@ -1,4 +1,13 @@
 package com.ludo.study.studymatchingplatform.user.service.dto.request;
 
-public record ChangeUserNicknameRequest(String changeNickname) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
+public record ChangeUserNicknameRequest(
+		@NotBlank
+		@NotEmpty
+		@Pattern(regexp = "^\\S(.*\\S)?$", message = "맨 앞, 맨 뒤는 공백일 수 없습니다.")
+		String changeNickname
+) {
 }
