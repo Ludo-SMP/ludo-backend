e7f40550429cc0284e56ba7eb0f3e1cbf0381fe3
june-777 <wlwhswnsrl96@gmail.com>
2024-03-06 18:29:05 +0900

    feat: Custom Exception이 BusinessException을 상속하도록 추가

--- src/main/java/com/ludo/study/studymatchingplatform/user/domain/exception/DuplicateNicknameException.java
+++ src/main/java/com/ludo/study/studymatchingplatform/user/domain/exception/DuplicateNicknameException.java
@@ -1,6 +1,8 @@
 package com.ludo.study.studymatchingplatform.user.domain.exception;
 
-public class DuplicateNicknameException extends RuntimeException {
+import com.ludo.study.studymatchingplatform.study.service.exception.BusinessException;
+
+public class DuplicateNicknameException extends BusinessException {
 
 	public static final String MESSAGE = "이미 존재하는 닉네임입니다.";
 
