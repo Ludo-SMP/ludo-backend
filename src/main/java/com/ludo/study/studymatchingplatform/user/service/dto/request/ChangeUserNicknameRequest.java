84ba269f5e1aeb66a5094168d2be17b486fababd
june-777 <wlwhswnsrl96@gmail.com>
2024-03-06 20:45:00 +0900

    feat: 문자열 크기 1 ~ 20 Bean Validation 추가

--- src/main/java/com/ludo/study/studymatchingplatform/user/service/dto/request/ChangeUserNicknameRequest.java
+++ src/main/java/com/ludo/study/studymatchingplatform/user/service/dto/request/ChangeUserNicknameRequest.java
@@ -3,11 +3,13 @@ package com.ludo.study.studymatchingplatform.user.service.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record ChangeUserNicknameRequest(
 		@NotBlank
 		@NotEmpty
 		@Pattern(regexp = "^\\S(.*\\S)?$", message = "맨 앞, 맨 뒤는 공백일 수 없습니다.")
+		@Size(min = 1, max = 20)
 		String changeNickname
 ) {
 }
