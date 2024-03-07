c1ad1adabf2ef61c3623f6c70215d3015f5d2e6c
Hugh <lee2317884@gmail.com>
2024-03-07 14:47:30 +0900

    SP-359 fix: 로그아웃시 리다이렉션 로직 제거

--- src/main/java/com/ludo/study/studymatchingplatform/auth/common/AuthController.java
+++ src/main/java/com/ludo/study/studymatchingplatform/auth/common/AuthController.java
@@ -1,7 +1,5 @@
 package com.ludo.study.studymatchingplatform.auth.common;
 
-import java.io.IOException;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +14,9 @@ public class AuthController {
 
 	private final CookieProvider cookieProvider;
 
-	private final Redirection redirection;
-
 	@PostMapping("/auth/logout")
-	public void logout(final HttpServletResponse response) throws IOException {
+	public void logout(final HttpServletResponse response) {
 		cookieProvider.clearAuthCookie(response);
-		redirection.to("/", response);
 	}
+
 }
