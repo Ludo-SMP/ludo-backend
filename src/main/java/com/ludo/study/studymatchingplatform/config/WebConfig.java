527ea7fe31010f773a09af594b291fef78a1ddae
Hugh <lee2317884@gmail.com>
2024-03-14 15:35:12 +0900

    SP-377 feat: CORS 예외 항목 nginx URL 추가

--- src/main/java/com/ludo/study/studymatchingplatform/config/WebConfig.java
+++ src/main/java/com/ludo/study/studymatchingplatform/config/WebConfig.java
@@ -27,7 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("https://local.ludoapi.store:3000")
+				.allowedOrigins("https://local.ludoapi.store:3000", "https://ludoapi.store")
 				.allowedMethods("*")
 				.allowedHeaders("*")
 				.allowCredentials(true)
