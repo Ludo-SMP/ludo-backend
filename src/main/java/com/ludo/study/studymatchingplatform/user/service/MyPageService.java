8d433e942e244477c600d38d8601807babf19280
Hugh <lee2317884@gmail.com>
2024-03-16 15:35:38 +0900

    SP-377 refactor: 마이 페이지 조회시 진행 완료 상태로 변경된 스터디 여부를 판별하는 메서드 추가

--- src/main/java/com/ludo/study/studymatchingplatform/user/service/MyPageService.java
+++ src/main/java/com/ludo/study/studymatchingplatform/user/service/MyPageService.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.ludo.study.studymatchingplatform.study.domain.recruitment.applicant.Applicant;
+import com.ludo.study.studymatchingplatform.study.domain.study.Study;
 import com.ludo.study.studymatchingplatform.study.domain.study.participant.Participant;
 import com.ludo.study.studymatchingplatform.study.repository.recruitment.applicant.ApplicantRepositoryImpl;
+import com.ludo.study.studymatchingplatform.study.repository.study.StudyRepositoryImpl;
 import com.ludo.study.studymatchingplatform.study.repository.study.participant.ParticipantRepositoryImpl;
 import com.ludo.study.studymatchingplatform.user.domain.user.User;
 import com.ludo.study.studymatchingplatform.user.service.dto.response.MyPageResponse;
@@ -19,12 +21,23 @@ public class MyPageService {
 
 	private final ParticipantRepositoryImpl participantRepository;
 	private final ApplicantRepositoryImpl applicantRepository;
+	private final StudyRepositoryImpl studyRepository;
 
 	public MyPageResponse retrieveMyPage(final User user) {
+		ensureStudyStatusCompleted(user);
 		final List<Participant> participants = retrieveParticipantStudies(user);
 		final List<Applicant> applicants = retrieveApplyRecruitment(user);
 		final List<Participant> completedStudies = retrieveCompletedStudy(user);
 		return MyPageResponse.from(user, participants, applicants, completedStudies);
+	}
+
+	private void ensureStudyStatusCompleted(final User user) {
+		final List<Participant> participants = participantRepository.findByUserId(user.getId());
+		for (Participant participant : participants) {
+			final Study study = participant.getStudy();
+			study.modifyStatusToCompleted();
+			studyRepository.save(study);
+		}
 	}
 
 	private List<Participant> retrieveParticipantStudies(final User user) {
