665ac6125afc214cfc06662b286506b35b62785e
Hugh <lee2317884@gmail.com>
2024-03-18 04:43:01 +0900

    SP-376 test: redis 정상 동작 테스트

---
+++ src/test/java/com/ludo/study/studymatchingplatform/RefreshTokenTest.java
@@ -0,0 +1,41 @@
+package com.ludo.study.studymatchingplatform;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ludo.study.studymatchingplatform.user.domain.user.redis.RefreshToken;
+import com.ludo.study.studymatchingplatform.user.repository.user.redis.RefreshTokenRepository;
+
+@SpringBootTest
+class RefreshTokenTest {
+
+	@Autowired
+	private RefreshTokenRepository refreshTokenRepository;
+
+	@AfterEach
+	void tearDown() throws Exception {
+		refreshTokenRepository.deleteAll();
+	}
+
+	@DisplayName("[Success] 리프레시 토큰 등록 조회 태스트")
+	@Test
+	void redisConnectionTest() {
+		// given
+		final Long id = 1L;
+		RefreshToken refreshToken = new RefreshToken(id, "refreshToken", 12345556L);
+
+		// when
+		refreshTokenRepository.save(refreshToken);
+
+		// then
+		RefreshToken savedRefreshToken = refreshTokenRepository.findById(id).get();
+		assertThat(savedRefreshToken.getUserId()).isEqualTo(1L);
+		assertThat(savedRefreshToken.getToken()).isEqualTo("refreshToken");
+	}
+
+}
