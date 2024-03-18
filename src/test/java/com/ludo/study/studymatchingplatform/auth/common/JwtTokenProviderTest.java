74596df30eca5b03d7f219814ca67e54660bee1b
Hugh <lee2317884@gmail.com>
2024-03-18 16:28:29 +0900

    SP-376 test: Claims accessToken 추가 테스트

--- src/test/java/com/ludo/study/studymatchingplatform/auth/common/JwtTokenProviderTest.java
+++ src/test/java/com/ludo/study/studymatchingplatform/auth/common/JwtTokenProviderTest.java
@@ -16,14 +16,14 @@ class JwtTokenProviderTest {
 
 	@Test
 	void jwt_access_token_생성() {
-		AuthUserPayload payload = AuthUserPayload.from(1L);
+		AuthUserPayload payload = AuthUserPayload.from(1L, "access");
 		String accessToken = jwtTokenProvider.createAccessToken(payload);
 		assertThat(accessToken).isNotNull();
 	}
 
 	@Test
 	void jwt_access_token_검증() {
-		AuthUserPayload payload = AuthUserPayload.from(1L);
+		AuthUserPayload payload = AuthUserPayload.from(1L, "access");
 		String accessToken = jwtTokenProvider.createAccessToken(payload);
 
 		assertThatCode(() -> jwtTokenProvider.verifyAuthTokenOrThrow(accessToken))
