6eb439d0befd441bba13ae19813b0105ae843e0e
Hugh <lee2317884@gmail.com>
2024-03-21 06:54:11 +0900

    core: dispatcher servlet 도달하기 전, filter 에서 발생하는 예외 처리 로직 추가

--- src/main/java/com/ludo/study/studymatchingplatform/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/ludo/study/studymatchingplatform/filter/JwtAuthenticationFilter.java
@@ -3,11 +3,14 @@ package com.ludo.study.studymatchingplatform.filter;
 import java.io.IOException;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ludo.study.studymatchingplatform.auth.common.AuthUserPayload;
 import com.ludo.study.studymatchingplatform.auth.common.provider.CookieProvider;
 import com.ludo.study.studymatchingplatform.auth.common.provider.JwtTokenProvider;
+import com.ludo.study.studymatchingplatform.common.advice.CommonResponse;
 import com.ludo.study.studymatchingplatform.study.service.exception.AuthenticationException;
 
 import io.jsonwebtoken.Claims;
@@ -32,23 +35,37 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 									final FilterChain filterChain) throws ServletException, IOException {
 		if (!request.getMethod().equals("OPTIONS")) {
 			final Optional<String> authToken = cookieProvider.getAuthToken(request);
+			// 토큰이 null 이면 예외 response 를 반환한다.
 			if (authToken.isEmpty()) {
-				throw new AuthenticationException("Authorization 쿠키가 없습니다.");
+				jwtExceptionHandler(response, HttpStatus.UNAUTHORIZED, "Authorization 쿠키가 없습니다.");
+				return;
 			}
 
-			Claims claims = null;
 			try {
-				claims = jwtTokenProvider.verifyAuthTokenOrThrow(authToken.get());
+				Claims claims = jwtTokenProvider.verifyAuthTokenOrThrow(authToken.get());
 				final AuthUserPayload payload = AuthUserPayload.from(claims);
 				request.setAttribute(AUTH_USER_PAYLOAD, payload);
-			} catch (final Exception e) {
+			} catch (final AuthenticationException e) {
+				// 만료되거나 잘못된 토큰일 경우 예외 response 를 반환한다.
 				cookieProvider.clearAuthCookie(response);
-				throw e;
+				jwtExceptionHandler(response, HttpStatus.UNAUTHORIZED, e.getMessage());
+				return;
 			}
-
 		}
-
 		filterChain.doFilter(request, response);
+	}
+
+	private void jwtExceptionHandler(final HttpServletResponse response, final HttpStatus status,
+									 final String message) {
+		response.setStatus(status.value());
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		try {
+			final String json = new ObjectMapper().writeValueAsString(CommonResponse.error(message));
+			response.getWriter().write(json);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
 	}
 
 }
