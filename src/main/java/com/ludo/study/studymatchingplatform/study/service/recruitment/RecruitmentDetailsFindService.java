15c75ba3580acd12c279813889ac2cc72aa43907
Hugh <lee2317884@gmail.com>
2024-03-21 00:25:55 +0900

    refactor: 지원한 모집 공고가 존재하지 않을시 BusinessException 발생하도록 변경

--- src/main/java/com/ludo/study/studymatchingplatform/study/service/recruitment/RecruitmentDetailsFindService.java
+++ src/main/java/com/ludo/study/studymatchingplatform/study/service/recruitment/RecruitmentDetailsFindService.java
@@ -7,6 +7,7 @@ import com.ludo.study.studymatchingplatform.study.domain.recruitment.Recruitment
 import com.ludo.study.studymatchingplatform.study.domain.study.Study;
 import com.ludo.study.studymatchingplatform.study.repository.recruitment.RecruitmentRepositoryImpl;
 import com.ludo.study.studymatchingplatform.study.service.dto.response.recruitment.RecruitmentDetailsResponse;
+import com.ludo.study.studymatchingplatform.study.service.exception.BusinessException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,7 +22,7 @@ public class RecruitmentDetailsFindService {
 	@Transactional
 	public RecruitmentDetailsResponse findRecruitmentDetails(final Long id) {
 		Recruitment recruitment = recruitmentRepository.findById(id)
-				.orElseThrow(() -> new IllegalArgumentException("지원 공고가 없습니다."));
+				.orElseThrow(() -> new BusinessException("지원 공고가 없습니다."));
 
 		recruitment.upHit();
 		Study study = recruitment.getStudy();
