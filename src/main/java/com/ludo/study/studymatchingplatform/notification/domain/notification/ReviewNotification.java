efe1b5720f7396884e0fa24859692098a17a5cb3
june-777 <wlwhswnsrl96@gmail.com>
2024-05-14 22:13:44 +0900

    feat: @ManyToOne Lazy 로딩 반영

--- src/main/java/com/ludo/study/studymatchingplatform/notification/domain/notification/ReviewNotification.java
+++ src/main/java/com/ludo/study/studymatchingplatform/notification/domain/notification/ReviewNotification.java
@@ -3,6 +3,7 @@ package com.ludo.study.studymatchingplatform.notification.domain.notification;
 import com.ludo.study.studymatchingplatform.user.domain.user.User;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
@@ -12,7 +13,7 @@ public class ReviewNotification extends Notification {
 	// TODO: 신뢰도 기능 브랜치 병합 후, Review 엔티티로 수정 필요
 	private Long actor;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id", nullable = false)
 	private User notifier;
 
