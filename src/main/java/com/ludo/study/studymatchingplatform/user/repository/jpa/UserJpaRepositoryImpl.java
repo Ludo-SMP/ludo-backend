2b46a1d74c1401085592127b34f32ca48ceda265
june-777 <wlwhswnsrl96@gmail.com>
2024-05-20 20:51:18 +0900

    remove: 사용하지 않는 파일 제거

--- src/main/java/com/ludo/study/studymatchingplatform/user/repository/jpa/UserJpaRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-package com.ludo.study.studymatchingplatform.user.repository.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ludo.study.studymatchingplatform.user.domain.user.User;
-
-public interface UserJpaRepositoryImpl extends JpaRepository<User, Long> {
-
-}
