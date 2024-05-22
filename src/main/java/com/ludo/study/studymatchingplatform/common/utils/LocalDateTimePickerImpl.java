4c5b00de2dc6c0705392ca94b553baf7b0dc97b3
zxcev <121966058+zxcev@users.noreply.github.com>
2024-05-22 22:46:42 +0900

    Update src/main/java/com/ludo/study/studymatchingplatform/common/utils/LocalDateTimePickerImpl.java
    Co-authored-by: June <68291395+june-777@users.noreply.github.com>

--- src/main/java/com/ludo/study/studymatchingplatform/common/utils/LocalDateTimePickerImpl.java
+++ src/main/java/com/ludo/study/studymatchingplatform/common/utils/LocalDateTimePickerImpl.java
@@ -10,6 +10,8 @@ import java.time.LocalDateTime;
 public class LocalDateTimePickerImpl implements LocalDateTimePicker {
 
     public LocalDateTime now() {
+        return LocalDateTime.now().truncatedTo(ChronoUnit.MICROS);
+    }
         return LocalDateTime.now();
     }
 }
