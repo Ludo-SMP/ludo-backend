be933715876aadd972ea80f57f50b8edda8469b9
june-777 <wlwhswnsrl96@gmail.com>
2024-05-23 20:38:16 +0900

    feat: ServerSentEvent 관련 커스텀 예외

---
+++ src/main/java/com/ludo/study/studymatchingplatform/notification/exception/NotExistSseEmitterException.java
@@ -0,0 +1,13 @@
+package com.ludo.study.studymatchingplatform.notification.exception;
+
+public class NotExistSseEmitterException extends RuntimeException {
+
+	public NotExistSseEmitterException(String message) {
+		super(message);
+	}
+
+	public NotExistSseEmitterException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
---
+++ src/main/java/com/ludo/study/studymatchingplatform/notification/exception/ServerSentEventSendException.java
@@ -0,0 +1,9 @@
+package com.ludo.study.studymatchingplatform.notification.exception;
+
+public class ServerSentEventSendException extends RuntimeException {
+
+	public ServerSentEventSendException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
