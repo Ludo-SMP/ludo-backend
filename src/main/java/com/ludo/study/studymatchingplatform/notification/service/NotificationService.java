58e3bbb37079f52d01ba9443177b93ddf2afe881
june-777 <wlwhswnsrl96@gmail.com>
2024-05-23 18:07:19 +0900

    feat: 스터디 지원 현황 알림, 모집공고 알림 저장 서비스 로직 추가

--- src/main/java/com/ludo/study/studymatchingplatform/notification/service/NotificationService.java
+++ src/main/java/com/ludo/study/studymatchingplatform/notification/service/NotificationService.java
@@ -1,11 +1,20 @@
 package com.ludo.study.studymatchingplatform.notification.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.ludo.study.studymatchingplatform.notification.domain.notification.NotificationEventType;
+import com.ludo.study.studymatchingplatform.notification.domain.notification.RecruitmentNotification;
+import com.ludo.study.studymatchingplatform.notification.domain.notification.StudyNotification;
 import com.ludo.study.studymatchingplatform.notification.repository.dto.RecruitmentNotifierCondition;
+import com.ludo.study.studymatchingplatform.notification.repository.notification.RecruitmentNotificationRepositoryImpl;
+import com.ludo.study.studymatchingplatform.notification.repository.notification.ReviewNotificationRepositoryImpl;
+import com.ludo.study.studymatchingplatform.notification.repository.notification.StudyNotificationRepositoryImpl;
 import com.ludo.study.studymatchingplatform.study.domain.recruitment.Recruitment;
+import com.ludo.study.studymatchingplatform.study.domain.study.Study;
+import com.ludo.study.studymatchingplatform.study.repository.recruitment.applicant.ApplicantRepositoryImpl;
 import com.ludo.study.studymatchingplatform.user.domain.user.User;
 import com.ludo.study.studymatchingplatform.user.repository.user.UserRepositoryImpl;
 
@@ -17,7 +26,17 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class NotificationService {
 
+	// for notifier search
 	private final UserRepositoryImpl userRepository;
+	private final ApplicantRepositoryImpl applicantRepository;
+
+	// for notification save & find
+	private final StudyNotificationRepositoryImpl studyNotificationRepository;
+	private final RecruitmentNotificationRepositoryImpl recruitmentNotificationRepository;
+	private final ReviewNotificationRepositoryImpl reviewNotificationRepository;
+
+	// for notification server sent events
+	private final SseEmitters sseEmitters;
 
 	public List<User> recruitmentNotice(final Recruitment recruitment) {
 		final RecruitmentNotifierCondition recruitmentNotifierCondition = new RecruitmentNotifierCondition(
@@ -28,16 +47,30 @@ public class NotificationService {
 		List<User> recruitmentNotifiers = userRepository.findRecruitmentNotifiers(recruitmentNotifierCondition);
 		log.info("recruitmentNotifiers = {}", recruitmentNotifiers);
 
+		// TODO: 알림 테이블에 저장 테스트
+		final List<RecruitmentNotification> recruitmentNotifications = recruitmentNotificationRepository.saveAll(
+				recruitmentNotifiers
+						.stream()
+						.map(notifier -> RecruitmentNotification.of(
+								NotificationEventType.RECRUITMENT, LocalDateTime.now(), recruitment, notifier))
+						.toList());
 		// TODO: 알림 대상자에게 SSE 실시간 알림 전송로직 추가
-		// TODO: 실시간 알림 전송 후 알림 테이블에 저장하는 로직 추가
+
 		return recruitmentNotifiers;
 	}
 
-	public void studyApplicantNotice(final Recruitment recruitment) {
-		final List<User> studyParticipantUsers = userRepository.findParticipantUsersByStudyId(recruitment.getStudyId());
+	public void studyApplicantNotice(final Study study) {
+		final List<User> studyParticipantUsers = userRepository.findParticipantUsersByStudyId(study.getId());
+
+		// TODO: 알림 테이블에 저장 테스트
+		final List<StudyNotification> studyNotifications = studyParticipantUsers
+				.stream()
+				.map(notifier -> new StudyNotification(
+						NotificationEventType.STUDY_APPLICANT, LocalDateTime.now(), study, notifier))
+				.toList();
+		studyNotificationRepository.saveAll(studyNotifications);
 
 		// TODO: 알림 대상자에게 SSE 실시간 알림 전송로직 추가
-		// TODO: 실시간 알림 전송 후 알림 테이블에 저장하는 로직 추가
 	}
 
 }
