0f09313caa6329a05b834821bd30ccf95623f581
june-777 <wlwhswnsrl96@gmail.com>
2024-05-23 18:10:00 +0900

    feat: 스터디 지원 결과/스터디 탈퇴 요청/스터디 탈퇴/리뷰 시작/리뷰 받음/상호 리뷰 알림 스켈레톤 코드 구현

--- src/main/java/com/ludo/study/studymatchingplatform/notification/service/NotificationService.java
+++ src/main/java/com/ludo/study/studymatchingplatform/notification/service/NotificationService.java
@@ -13,6 +13,8 @@ import com.ludo.study.studymatchingplatform.notification.repository.notification
 import com.ludo.study.studymatchingplatform.notification.repository.notification.ReviewNotificationRepositoryImpl;
 import com.ludo.study.studymatchingplatform.notification.repository.notification.StudyNotificationRepositoryImpl;
 import com.ludo.study.studymatchingplatform.study.domain.recruitment.Recruitment;
+import com.ludo.study.studymatchingplatform.study.domain.recruitment.applicant.Applicant;
+import com.ludo.study.studymatchingplatform.study.domain.recruitment.applicant.ApplicantStatus;
 import com.ludo.study.studymatchingplatform.study.domain.study.Study;
 import com.ludo.study.studymatchingplatform.study.repository.recruitment.applicant.ApplicantRepositoryImpl;
 import com.ludo.study.studymatchingplatform.user.domain.user.User;
@@ -71,6 +73,77 @@ public class NotificationService {
 		studyNotificationRepository.saveAll(studyNotifications);
 
 		// TODO: 알림 대상자에게 SSE 실시간 알림 전송로직 추가
+	}
+
+	public void studyApplicantResultNotice(final Recruitment recruitment, final User user) {
+		final Applicant applicant = applicantRepository.find(recruitment.getId(), user.getId())
+				.orElseThrow(() -> new IllegalStateException("지원하지 않은 사용자입니다."));
+
+		// TODO: 실시간 알림 전송 후 알림 테이블에 저장하는 로직 추가
+		if (applicant.statusIsEqualTo(ApplicantStatus.ACCEPTED)) {
+
+		}
+
+		if (applicant.statusIsEqualTo(ApplicantStatus.REFUSED)) {
+
+		}
+		// TODO: 알림 대상자에게 SSE 실시간 알림 전송로직 추가
+
+	}
+
+	public void studyParticipantLeaveNotice(final Study study) {
+		final List<User> studyParticipantUsers = userRepository.findParticipantUsersByStudyId(study.getId());
+
+		// TODO: 알림 테이블에 저장 테스트
+		final List<StudyNotification> studyNotifications = studyParticipantUsers
+				.stream()
+				.map(notifier -> new StudyNotification(
+						NotificationEventType.STUDY_PARTICIPANT_LEAVE, LocalDateTime.now(), study, notifier))
+				.toList();
+		studyNotificationRepository.saveAll(studyNotifications);
+
+		// TODO: 알림 대상자에게 SSE 실시간 알림 전송로직 추가
+	}
+
+	public void studyParticipantLeaveApplyNotice(final Study study) {
+		final User studyOwner = study.getOwner();
+
+		// TODO: 알림 테이블에 저장 테스트
+		StudyNotification studyNotification = new StudyNotification(
+				NotificationEventType.STUDY_PARTICIPANT_LEAVE_APPLY, LocalDateTime.now(), study, studyOwner);
+		studyNotificationRepository.save(studyNotification);
+
+		// TODO: 알림 대상자에게 SSE 실시간 알림 전송로직 추가
+	}
+
+	public void reviewStartNotice(final Study study) {
+		final List<User> studyParticipantUsers = userRepository.findParticipantUsersByStudyId(study.getId());
+
+		// TODO: 실시간 알림 전송 후 알림 테이블에 저장하는 로직 추가. 리뷰 기능 완성돼야 진행 가능
+		// TODO: 알림 대상자에게 SSE 실시간 알림 전송로직 추가
+	}
+
+	/**
+	 * TODO: 리뷰 기능 구현 완료 후 진행
+	 * @param: maybe Review Domain Entity
+	 * 리뷰 테이블 조회 로직이 필요한데, 중복 구현 가능성
+	 */
+	public void reviewReceiveNotice() {
+	}
+
+	/**
+	 * TODO: 리뷰 기능 구현 완료 후 진행
+	 * @param: maybe Review Domain Entity
+	 * 리뷰 테이블 조회 로직이 필요한데, 중복 구현 가능성
+	 */
+	public void reviewPeerFinishNotice(final Study study) {
+
+	}
+
+	// TODO: @Scheduled 적용
+	public void studyEndDateNotice() {
+		// 종료 기간까지 N일 남은 스터디 조회
+		// 해당 스터디의 방장 조회
 	}
 
 }
