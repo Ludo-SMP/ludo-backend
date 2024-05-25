2a2dbd6df0216f260dcc9ecc578e2c36555c6294
🌸 🤗 <x@x.local>
2024-05-25 22:13:34 +0900

    feat: UtcDateTimerPicker 및 JPA DateTime 관련 Audit 작성 및 commit aceept로 인한 오류 수정

--- src/main/java/com/ludo/study/studymatchingplatform/common/entity/BaseEntity.java
+++ src/main/java/com/ludo/study/studymatchingplatform/common/entity/BaseEntity.java
@@ -1,51 +1,46 @@
 package com.ludo.study.studymatchingplatform.common.entity;
 
-import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
+import com.ludo.study.studymatchingplatform.common.auditing.PrePersistUtcNow;
+import com.ludo.study.studymatchingplatform.common.auditing.PreUpdateUtcNow;
+import com.ludo.study.studymatchingplatform.common.auditing.UtcNowAuditor;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
 
 @Getter
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
+@EntityListeners(UtcNowAuditor.class)
 @ToString(of = "createdDateTime")
 public abstract class BaseEntity {
 
-	@CreatedDate
-	@Column(nullable = false, updatable = false)
-	private LocalDateTime createdDateTime;
+    @PrePersistUtcNow
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDateTime;
 
-	@LastModifiedDate
-	@Column(nullable = false)
-	private LocalDateTime updatedDateTime;
+    @PreUpdateUtcNow
+    @Column(nullable = false)
+    private LocalDateTime updatedDateTime;
 
-	@Column(nullable = true)
-	@Builder.Default
-	private LocalDateTime deletedDateTime = null;
+    @Column(nullable = true)
+    @Builder.Default
+    private LocalDateTime deletedDateTime = null;
 
-	public boolean isDeleted() {
-		return deletedDateTime != null;
-	}
+    public boolean isDeleted() {
+        return deletedDateTime != null;
+    }
 
-	public void softDelete() {
-		deletedDateTime = LocalDateTime.now();
-	}
+    public void softDelete() {
+        deletedDateTime = LocalDateTime.now();
+    }
 
-	public void activate() {
-		deletedDateTime = null;
-	}
+    public void activate() {
+        deletedDateTime = null;
+    }
 
 }
--- src/main/java/com/ludo/study/studymatchingplatform/common/utils/UtcDateTimePicker.java
+++ src/main/java/com/ludo/study/studymatchingplatform/common/utils/UtcDateTimePicker.java
@@ -2,6 +2,32 @@ package com.ludo.study.studymatchingplatform.common.utils;
 
 import java.time.LocalDateTime;
 
+/**
+ * <h5>테스트 시, Profile을 "test"로 설정하면 항상 2000-01-01으로 고정된 DateTime 획득 </h5>
+ * <h5>Profile가 "test"가 아닐 경우, `now`는 현재 시각 반환. 단, UTC+00에 고정되며, microseconds 이하는 truncate</h5>
+ *
+ *
+ *
+ * <h4> ========Example======= </h3>
+ * <pre>
+ * &#064;SpringBootTest
+ * &#064;ActiveProfiles("test")
+ * public class Test {
+ *     &#064;Autowired
+ *     UtcDateTimePicker utcDateTimePicker;
+ *
+ *     &#064;Test
+ *     void test() {
+ *         // 2000-01-01 고정 UTC Time
+ *         LocalDateTime dt = utcDateTimePicker.now();
+ *     }
+ *
+ * }
+ * </pre>
+ *
+ * @see com.ludo.study.studymatchingplatform.study.service.study.FixedUtcDateTimePicker
+ * @see CurrentUtcDateTimePicker
+ */
 public interface UtcDateTimePicker {
     LocalDateTime now();
 }
