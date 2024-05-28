48e286f7903cf93ca6c40e775caef91c04503d7d
june-777 <wlwhswnsrl96@gmail.com>
2024-05-28 18:44:55 +0900

    fix: suggestion 수락 과정에서 코드 병합 충돌 이슈 해결

--- src/main/java/com/ludo/study/studymatchingplatform/common/utils/LocalDateTimePickerImpl.java
+++ src/main/java/com/ludo/study/studymatchingplatform/common/utils/LocalDateTimePickerImpl.java
@@ -1,19 +1,17 @@
 package com.ludo.study.studymatchingplatform.common.utils;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
 
 @Profile("!test")
 @Component
 public class LocalDateTimePickerImpl implements LocalDateTimePicker {
 
-    public LocalDateTime now() {
-        return LocalDateTime.now().truncatedTo(ChronoUnit.MICROS);
-    }
-        return LocalDateTime.now().truncatedTo(ChronoUnit.MICROS);
-    }
-        return LocalDateTime.now();
-    }
+	public LocalDateTime now() {
+		return LocalDateTime.now().truncatedTo(ChronoUnit.MICROS);
+	}
+    
 }
