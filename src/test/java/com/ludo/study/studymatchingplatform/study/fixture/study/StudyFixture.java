e75e4665ce304624f61c11975752dc88b0cfeb1c
june-777 <wlwhswnsrl96@gmail.com>
2024-06-10 17:05:04 +0900

    feat: 테스트 픽스쳐 수정 및 카테고리 데이터 중복 저장 setup 삭제

--- src/test/java/com/ludo/study/studymatchingplatform/study/fixture/study/StudyFixture.java
+++ src/test/java/com/ludo/study/studymatchingplatform/study/fixture/study/StudyFixture.java
@@ -3,44 +3,104 @@ package com.ludo.study.studymatchingplatform.study.fixture.study;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
+import com.ludo.study.studymatchingplatform.common.utils.UtcDateTimePicker;
 import com.ludo.study.studymatchingplatform.study.domain.study.Platform;
 import com.ludo.study.studymatchingplatform.study.domain.study.Study;
 import com.ludo.study.studymatchingplatform.study.domain.study.StudyStatus;
 import com.ludo.study.studymatchingplatform.study.domain.study.Way;
 import com.ludo.study.studymatchingplatform.study.domain.study.category.Category;
 import com.ludo.study.studymatchingplatform.study.fixture.study.category.CategoryFixture;
+import com.ludo.study.studymatchingplatform.study.service.study.FixedUtcDateTimePicker;
 import com.ludo.study.studymatchingplatform.user.domain.user.User;
-import com.ludo.study.studymatchingplatform.user.fixture.user.UserFixture;
 
 public class StudyFixture {
 
-	public static final Study USER1_PROJECT_ONLINE_STUDY = Study.builder()
-			.title("프로젝트 온라인 스터디 제목")
-			.owner(UserFixture.user1)
-			.category(CategoryFixture.CATEGORY_PROJECT)
-			.status(StudyStatus.RECRUITING)
-			.way(Way.ONLINE)
-			.platform(Platform.GATHER)
-			.platformUrl("platform url")
-			.participantCount(1)
-			.participantLimit(5)
-			.startDateTime(LocalDateTime.now().truncatedTo(ChronoUnit.MICROS))
-			.endDateTime(LocalDateTime.now().plusMonths(1).truncatedTo(ChronoUnit.MICROS))
-			.build();
+	public static Study PROJECT_ONLINE_STUDY(User owner, UtcDateTimePicker utcDateTimePicker) {
 
-	public static final Study study1 = Study.builder()
-			.title("스터디1 제목")
-			.owner(UserFixture.user1)
-			.category(CategoryFixture.CATEGORY_PROJECT)
-			.status(StudyStatus.RECRUITING)
-			.way(Way.ONLINE)
-			.platform(Platform.GATHER)
-			.platformUrl("platform url")
-			.participantCount(1)
-			.participantLimit(5)
-			.startDateTime(LocalDateTime.now().truncatedTo(ChronoUnit.MICROS))
-			.endDateTime(LocalDateTime.now().plusMonths(1).truncatedTo(ChronoUnit.MICROS))
-			.build();
+		return Study.builder()
+				.title("프로젝트 온라인 스터디 제목")
+				.owner(owner)
+				.category(CategoryFixture.CATEGORY_PROJECT)
+				.status(StudyStatus.RECRUITING)
+				.way(Way.ONLINE)
+				.platform(Platform.GATHER)
+				.platformUrl("platform url")
+				.participantCount(1)
+				.participantLimit(5)
+				.startDateTime(utcDateTimePicker.now().truncatedTo(ChronoUnit.MICROS))
+				.endDateTime(utcDateTimePicker.now().plusMonths(1).truncatedTo(ChronoUnit.MICROS))
+				.build();
+	}
+
+	public static Study STUDY1(User owner, LocalDateTime startDateTime, LocalDateTime endDateTime) {
+
+		return Study.builder()
+				.title("스터디1 제목")
+				.owner(owner)
+				.category(CategoryFixture.CATEGORY_PROJECT)
+				.status(StudyStatus.RECRUITING)
+				.way(Way.ONLINE)
+				.platform(Platform.GATHER)
+				.platformUrl("platform url")
+				.participantCount(1)
+				.participantLimit(5)
+				.startDateTime(startDateTime)
+				.endDateTime(endDateTime.truncatedTo(ChronoUnit.MICROS))
+				.build();
+	}
+
+	public static Study STUDY2(User owner, LocalDateTime startDateTime, LocalDateTime endDateTime) {
+
+		return Study.builder()
+				.title("스터디2 제목")
+				.owner(owner)
+				.category(CategoryFixture.CATEGORY_PROJECT)
+				.status(StudyStatus.RECRUITING)
+				.way(Way.ONLINE)
+				.platform(Platform.GATHER)
+				.platformUrl("platform url")
+				.participantCount(1)
+				.participantLimit(5)
+				.startDateTime(startDateTime)
+				.endDateTime(endDateTime.truncatedTo(ChronoUnit.MICROS))
+				.build();
+	}
+
+	public static Study STUDY3(User owner, LocalDateTime startDateTime, LocalDateTime endDateTime) {
+		FixedUtcDateTimePicker fixedUtcDateTimePicker = new FixedUtcDateTimePicker();
+
+		return Study.builder()
+				.title("스터디3 제목")
+				.owner(owner)
+				.category(CategoryFixture.CATEGORY_PROJECT)
+				.status(StudyStatus.RECRUITING)
+				.way(Way.ONLINE)
+				.platform(Platform.GATHER)
+				.platformUrl("platform url")
+				.participantCount(1)
+				.participantLimit(5)
+				.startDateTime(startDateTime)
+				.endDateTime(endDateTime.truncatedTo(ChronoUnit.MICROS))
+				.build();
+	}
+
+	public static Study STUDY4(User owner, LocalDateTime startDateTime, LocalDateTime endDateTime) {
+		FixedUtcDateTimePicker fixedUtcDateTimePicker = new FixedUtcDateTimePicker();
+
+		return Study.builder()
+				.title("스터디4 제목")
+				.owner(owner)
+				.category(CategoryFixture.CATEGORY_PROJECT)
+				.status(StudyStatus.RECRUITING)
+				.way(Way.ONLINE)
+				.platform(Platform.GATHER)
+				.platformUrl("platform url")
+				.participantCount(1)
+				.participantLimit(5)
+				.startDateTime(startDateTime)
+				.endDateTime(endDateTime.truncatedTo(ChronoUnit.MICROS))
+				.build();
+	}
 
 	public static Study createStudy(StudyStatus studyStatus, String title, Way way, Category category, User user,
 									int participantCount, int participantLimit, Platform platform, String platformUrl
