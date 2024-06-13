144a5190bae1c0a4e1190479d955b994759c6c6d
june-777 <wlwhswnsrl96@gmail.com>
2024-06-13 19:10:31 +0900

    feat: 서블릿 필터의 인증 엔드포인트에 알림 엔드포인트 추가

--- src/main/java/com/ludo/study/studymatchingplatform/config/ServletFilterConfig.java
+++ src/main/java/com/ludo/study/studymatchingplatform/config/ServletFilterConfig.java
@@ -1,41 +1,44 @@
 package com.ludo.study.studymatchingplatform.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.ludo.study.studymatchingplatform.auth.common.provider.CookieProvider;
 import com.ludo.study.studymatchingplatform.auth.common.provider.JwtTokenProvider;
 import com.ludo.study.studymatchingplatform.auth.common.service.UserDetailsService;
 import com.ludo.study.studymatchingplatform.filter.JwtAuthenticationFilter;
 import com.ludo.study.studymatchingplatform.user.service.UserService;
+
 import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 public class ServletFilterConfig {
 
-    private final JwtTokenProvider jwtTokenProvider;
-    private final CookieProvider cookieProvider;
-    private final UserDetailsService userDetailsService;
-    private final UserService userService;
+	private final JwtTokenProvider jwtTokenProvider;
+	private final CookieProvider cookieProvider;
+	private final UserDetailsService userDetailsService;
+	private final UserService userService;
 
-    @Bean
-    public FilterRegistrationBean<Filter> jwtAuthenticationFilter() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+	@Bean
+	public FilterRegistrationBean<Filter> jwtAuthenticationFilter() {
+		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
 
-        filterRegistrationBean.setFilter(
-                new JwtAuthenticationFilter(jwtTokenProvider, cookieProvider, userDetailsService, userService));
-        filterRegistrationBean.setOrder(1);
-        addAuthenticationEndpoints(filterRegistrationBean);
-        return filterRegistrationBean;
-    }
+		filterRegistrationBean.setFilter(
+				new JwtAuthenticationFilter(jwtTokenProvider, cookieProvider, userDetailsService, userService));
+		filterRegistrationBean.setOrder(1);
+		addAuthenticationEndpoints(filterRegistrationBean);
+		return filterRegistrationBean;
+	}
 
-    private void addAuthenticationEndpoints(FilterRegistrationBean<Filter> filterRegistrationBean) {
-        filterRegistrationBean.addUrlPatterns("/api/users/*");
-        filterRegistrationBean.addUrlPatterns("/api/studies/*");
-        filterRegistrationBean.addUrlPatterns("/api/statistics/*");
-        filterRegistrationBean.addUrlPatterns("/api/reviews/*");
-    }
+	private void addAuthenticationEndpoints(FilterRegistrationBean<Filter> filterRegistrationBean) {
+		filterRegistrationBean.addUrlPatterns("/api/users/*");
+		filterRegistrationBean.addUrlPatterns("/api/studies/*");
+		filterRegistrationBean.addUrlPatterns("/api/statistics/*");
+		filterRegistrationBean.addUrlPatterns("/api/reviews/*");
+		filterRegistrationBean.addUrlPatterns("/api/notifications/*");
+	}
 
 }
