e6a010821bc4bdac0e6ff2fc350fa7ff0568e242
june-777 <wlwhswnsrl96@gmail.com>
2024-06-13 19:21:30 +0900

    feat: 알림관심키워드 엔티티의 기본키를 조합키로 변경
    변경 이유
    1. 알림관심키워드카테고리는 user_id, category_id 컬럼으로 구성되어 있고, 두 컬럼의 조합으로 레코드를 유일하게 식별함 (다른 알림키워드도 마찬가지)
    2. 알림관심키워드 설정 기능의 경우 기존에 사용자가 저장한 설정값과 요청으로 받은 설정값을 비교하여 필터하는 로직이 필요한데, 기존의 Identity 기본키 전략으로는 제약이 있음
    
    특이사항
    1. 알림관심키워드의 동등성 비교를 위해 조합키를 기반으로 equals & hascode 오버라이딩함
    2. 조합키가 null인 인스턴스 생성을 방지하기 위해 정적 메서드 패턴을 활용하여 이를 규약함

---
+++ src/main/java/com/ludo/study/studymatchingplatform/notification/domain/keyword/NotificationKeywordCategoryId.java
@@ -0,0 +1,20 @@
+package com.ludo.study.studymatchingplatform.notification.domain.keyword;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NotificationKeywordCategoryId implements Serializable {
+
+	private Long userId;
+	private Long categoryId;
+
+}
