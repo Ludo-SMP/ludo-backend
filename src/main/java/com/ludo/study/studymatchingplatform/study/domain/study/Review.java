bac5795397504d25d91656bdcd36b7029305d165
june-777 <wlwhswnsrl96@gmail.com>
2024-06-17 20:08:28 +0900

    refactor: 도메인 엔티티 컬럼 이름 변경

--- src/main/java/com/ludo/study/studymatchingplatform/study/domain/study/Review.java
+++ src/main/java/com/ludo/study/studymatchingplatform/study/domain/study/Review.java
@@ -5,6 +5,7 @@ import static jakarta.persistence.FetchType.*;
 import com.ludo.study.studymatchingplatform.common.entity.BaseEntity;
 import com.ludo.study.studymatchingplatform.user.domain.user.User;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,6 +29,7 @@ public class Review extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "review_id")
 	private Long id;
 
 	@ManyToOne(fetch = LAZY)
