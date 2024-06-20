d1d0764241d81521462c16fc37a2293f3d61a9bf
Hugh <lee2317884@gmail.com>
2024-06-20 20:12:09 +0900

    SP-570 refactor: StudyStatistics / ReviewStatistics, Repository 네이밍 컨벤션 반영

---
+++ src/main/java/com/ludo/study/studymatchingplatform/study/repository/study/StudyStatisticsRepositoryImpl.java
@@ -0,0 +1,36 @@
+package com.ludo.study.studymatchingplatform.study.repository.study;
+
+import static com.ludo.study.studymatchingplatform.study.domain.study.QStudyStatistics.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.ludo.study.studymatchingplatform.study.domain.study.StudyStatistics;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class StudyStatisticsRepositoryImpl {
+
+	private final JPAQueryFactory q;
+	private final StudyStatisticsJpaRepository studyStatisticsJpaRepository;
+
+	public List<StudyStatistics> findByUserIdsIn(final List<Long> userIds) {
+		//        return q.selectFrom();
+		return null;
+	}
+
+	public Optional<StudyStatistics> findByUserId(final Long userId) {
+		return Optional.ofNullable(q.selectFrom(studyStatistics)
+				.where(studyStatistics.user.id.eq(userId))
+				.fetchOne());
+	}
+
+	public StudyStatistics save(final StudyStatistics statistics) {
+		return studyStatisticsJpaRepository.save(statistics);
+	}
+}
